5bb149cb76af0e28e312b95b0a719fb0d22ff94b
RoanRatering <rateringroan@gmail.com>
2023-08-22 11:38:39 +0200

    Why is this commit so big?
    small changes actually

--- CaveDiving/Assets/FollowObjectWithOffset.cs
+++ CaveDiving/Assets/FollowObjectWithOffset.cs
@@ -23,21 +23,28 @@ public class FollowObjectWithOffset : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            isFollowing = true;
-            initialOffset = objectToFollow.transform.position - referenceObject.transform.position;
-        }
+        if (!e.GetComponent<SphereFollowMouse>().getIsBeyondMaxDistance())
+        { 
+            if (Input.GetMouseButtonDown(0))
+            {
+                isFollowing = true;
+                initialOffset = objectToFollow.transform.position - referenceObject.transform.position;
+            }
 
-        if (Input.GetMouseButtonUp(0))
+            if (Input.GetMouseButtonUp(0))
+            {
+                isFollowing = false;
+            }
+
+            if (isFollowing)
+            {
+                Vector3 targetPosition = referenceObject.transform.position + initialOffset;
+                objectToFollow.transform.position = Vector3.Lerp(objectToFollow.transform.position, targetPosition, Time.deltaTime * 5f);
+            }
+        }
+        else
         {
             isFollowing = false;
-        }
-
-        if (isFollowing && !e.GetComponent<SphereFollowMouse>().getIsBeyondMaxDistance())
-        {
-            Vector3 targetPosition = referenceObject.transform.position + initialOffset;
-            objectToFollow.transform.position = Vector3.Lerp(objectToFollow.transform.position, targetPosition, Time.deltaTime * 5f);
         }
     }
 }
